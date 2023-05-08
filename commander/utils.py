11f2c4b80cbc6d57ae37dd0364b07cb959874aa7
ehsan <1374ea@gmail.com>
2023-05-08 14:03:25 +0330

    Better structure for static files

--- commander/utils.py
+++ commander/utils.py
@@ -14,17 +14,20 @@ def asset(*args, **kwargs):
     return _standard_directories_content_path_generator('assets', *args, **kwargs)
 
 
-def resource(*args, **kwargs):
-    return _standard_directories_content_path_generator('resources', *args, **kwargs)
+def temp(*args, **kwargs):
+    kwargs.update({'mkdir': True})
+
+    return _standard_directories_content_path_generator('temps', *args, **kwargs)
 
 
 def result(*args, **kwargs):
+    kwargs.update({'mkdir': True})
+
     return _standard_directories_content_path_generator('results', *args, **kwargs)
 
 
 def _standard_directories_content_path_generator(standard_directory_name, *args, **kwargs):
-    topic_dir = os.environ['RUNNING_TOPIC_DIR']
-    topic_standard_directory_path = os.path.join(topic_dir, standard_directory_name)
+    topic_standard_directory_path = os.path.join(topic_dir_path(), standard_directory_name, action_name())
     generated_path = os.path.join(topic_standard_directory_path, *args)
 
     if kwargs.get('mkdir'):
